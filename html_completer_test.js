bbe81afed12cc0af444d683c6e3a38b1a7752c09
Fabian Jakobs <fabian@c9.io>
2015-04-15 14:05:15 +0000

    convert tests to mocha

--- html_completer_test.js
+++ html_completer_test.js
@@ -1,62 +1,57 @@
+"use strict";
 "use server";
+"use mocha";
 
+require("c9/inline-mocha")(module);
+require("../../test/setup_paths");
 if (typeof process !== "undefined") {
     require("amd-loader");
-    require("../../test/setup_paths");
 }
-
-define(function(require, exports, module) {
 
 var assert = require("ace/test/assertions");
 var Document = require("ace/document").Document;
 var completer = require("plugins/c9.ide.language.html/html_completer");
 
-module.exports = {
+describe(__filename, function() {
     
-    "test basic snippet completion 1" : function(next) {
+    it("basic snippet completion 1", function(next) {
         var doc = new Document("<bo");
-        var matches = completer.complete(doc, null, {row: 0, column: 3}, null, function(matches) {
+        completer.complete(doc, null, {row: 0, column: 3}, null, function(matches) {
             console.log("Matches:", matches);
             assert.equal(matches.length, 1);
             assert.equal(matches[0].name, "body");
             next();
         });
-    },
+    });
 
-    "test basic snippet completion 2" : function(next) {
+    it("basic snippet completion 2", function(next) {
         var doc = new Document("link");
-        var matches = completer.complete(doc, null, {row: 0, column: 3}, null, function(matches) {
+        completer.complete(doc, null, {row: 0, column: 3}, null, function(matches) {
             console.log("Matches:", matches);
             assert.equal(matches.length, 1);
             assert.ok(matches[0].replaceText.match(/^<link .*stylesheet.*css.*\/>$/));
             next();
         });
-    },
+    });
 
-    "test Jade/Haml completion 1" : function(next) {
+    it("Jade/Haml completion 1", function(next) {
         var doc = new Document("\n.breaty");
-        var matches = completer.complete(doc, null, {row: 1, column: 7}, null, function(matches) {
+        completer.complete(doc, null, {row: 1, column: 7}, null, function(matches) {
             console.log("Matches:", matches);
             assert.equal(matches.length, 1);
             assert.equal(matches[0].replaceText, '<div class="breaty">^^</div>');
             next();
         });
-    },
+    });
 
-    "test Jade/Haml completion 2" : function(next) {
+    it("Jade/Haml completion 2", function(next) {
         var doc = new Document("<span>anything</span>table.cool<p>stuff</p>");
-        var matches = completer.complete(doc, null, {row: 0, column: 31}, null, function(matches) {
+        completer.complete(doc, null, {row: 0, column: 31}, null, function(matches) {
             console.log("Matches:", matches);
             assert.equal(matches.length, 1);
             assert.ok(matches[0].replaceText.match(/^<table/));
             assert.ok(matches[0].replaceText.match(/<\/table>$/));
             next();
         });
-    }
-};
-
+    });
 });
-
-if (typeof module !== "undefined" && module === require.main) {
-    require("asyncjs").test.testcase(module.exports).exec();
-}
